8dc86708685a2b22e461c3d07e6335d7fe34ec44
Siam Hossain <mdsiamh77@gmail.com>
2026-08-04 21:34:24 +0600

    add Allowed Hosts

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,11 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(",")])
+ALLOWED_HOSTS = ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "https://nea-pure.vercel.app/",
+]
 
 # -------------------------------------------------
 # Custom User Model
